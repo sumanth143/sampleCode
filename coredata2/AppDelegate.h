2b3c9de81bcd99b83b34ca77cd6ecf34ef7f3c2c
student <student@achiever-119.local>
2017-08-19 17:26:57 +0530

    Initial Commit

---
+++ coredata2/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  coredata2
+//
+//  Created by student on 8/19/17.
+//  Copyright © 2017 vinod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ coredata2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coredata2
+//
+//  Created by student on 8/19/17.
+//  Copyright © 2017 vinod. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
